e824272d9fce898d97238b7218d87d290001693b
José Rodolfo <j.rodolfopps@hotmail.com>
2020-05-14 10:17:17 -0300

    NEW: Add models from users and tweets

---
+++ src/models/User.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const User = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      text: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      max: 1024,
+      select: false
+    },
+    avatar: {
+      type: String,
+    },
+    cover: {
+      type: String,
+    },
+    tweets: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Tweet",
+      },
+    ],
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", User);
