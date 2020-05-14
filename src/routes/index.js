bc817d836a8cfe029cea027279deba853493360d
José Rodolfo <j.rodolfopps@hotmail.com>
2020-05-14 11:06:21 -0300

    NEW: Add profiler update data

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 const { Router } = require("express");
 
 const auth = require("./auth")
+const profilerUpdate = require("./profiler/update")
 
 const router = new Router();
 
 router.use(auth.register);
 router.use(auth.login);
 router.use(auth.logout);
+router.use(profilerUpdate)
 
 module.exports = router;
---
+++ src/routes/profiler/update.js
@@ -0,0 +1,63 @@
+const { Router } = require("express");
+
+const auth = require("../../middlewares/auth");
+const User = require("../../models/User");
+
+const router = new Router();
+
+router.post("/users/me/update", auth, async (req, res) => {
+  try {
+    const { name, description } = req.body;
+
+    const user = await User.findById(req.user._id);
+
+    user.name = name;
+    user.description = description;
+
+    if (!!req.files) {
+      // Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
+      const avatar = req.files.avatar;
+      const cover = req.files.cover;
+
+      if (!!avatar) {
+        const filenameAvatar = `avatars/${avatar.name}`;
+        const avatarDir = `./uploads/${filenameAvatar}`;
+        avatar.mv(avatarDir);
+
+        user.avatar = filenameAvatar;
+      }
+
+      if (!!cover) {
+        const filenameCover = `covers/${cover.name}`;
+        const coverDir = `./uploads/${filenameCover}`;
+        cover.mv(coverDir);
+
+        user.cover = filenameCover;
+      }
+
+      //Use the mv() method to place the file in upload directory (i.e. "uploads")
+
+      // send response
+      // res.send({
+      //   status: true,
+      //   message: "File is uploaded",
+      //   data: {
+      //     name: avatar.name,
+      //     mimetype: avatar.mimetype,
+      //     size: avatar.size,
+      //   },
+      // });
+    }
+
+    user.save();
+
+    req.user = user;
+
+    res.send(user);
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+});
+
+module.exports = router;
+//https://attacomsian.com/blog/uploading-files-nodejs-express
